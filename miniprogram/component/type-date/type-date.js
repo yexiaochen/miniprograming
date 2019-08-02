4155cd715bd4914c0143a59d0aca4df5d6fe32df
yexiaochen <2277438436@qq.com>
2019-08-02 22:29:10 +0800

    type-monty

--- miniprogram/component/type-date/type-date.js
+++ miniprogram/component/type-date/type-date.js
@@ -1,1 +1,20 @@
-Component({})
+Component({
+  properties: {
+    componentHeight: Number
+  },
+  data: {
+    dateType: 0, // 0: today, 1: tomorrow
+    TDToDoList: [], // today Todo list
+    TMToDoList: [] // tomorrow Todo List
+  },
+  methods: {
+    selectType(event){
+      let data = event.currentTarget.dataset;
+      let {type} = data;
+      type = Number(!(+type));
+      this.setData({
+        dateType: type
+      })
+    }
+  }
+})
--- miniprogram/pages/year/year.js
+++ miniprogram/pages/year/year.js
@@ -3,6 +3,7 @@ Page({
   data: {
     yearHeight: 0,
     monthHeight: 0,
+    dateHeight: 0,
     // change tab
     activatedIndex: 0,
     targetType: [
@@ -33,19 +34,22 @@ Page({
 
   onLoad: function () {
     let height = {};
+    let yearChartHeight,monthChartHeight,componentHeight;
     let query = wx.createSelectorQuery().in(this); // Note: 操作节点
     query.selectAll('#little_target, #target_type, #target_content').boundingClientRect(
       res => {
         res.map(item => {
           height[`${item.id}_height`] = item.height;
         })
-        let yearChartHeight = height.little_target_height - height.target_content_height - height.target_type_height - 20;
+        yearChartHeight = height.little_target_height - height.target_content_height - height.target_type_height - 20;
 
-        let monthChartHeight = height.little_target_height - height.target_type_height - 48;
-        
+        componentHeight = monthChartHeight = height.little_target_height - height.target_type_height - 48;
+
+
         this.setData({
           yearHeight: yearChartHeight,
-          monthHeight: monthChartHeight
+          monthHeight: monthChartHeight,
+          dateHeight: componentHeight,
         })
       }
     ).exec();
