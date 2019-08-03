059c27329c422eaac253ccaa73d110ad5de9499f
cyril <cyril_dli@vipabc.com>
2019-08-03 15:57:03 +0800

    type-date

--- miniprogram/component/type-date/type-date.js
+++ miniprogram/component/type-date/type-date.js
@@ -3,17 +3,75 @@ Component({
     componentHeight: Number
   },
   data: {
+    inputFocus: true,
+    modifyType: 0, // 0: add, 1: fix;
+    fixIndex: 0,
     dateType: 0, // 0: today, 1: tomorrow
-    TDToDoList: [], // today Todo list
-    TMToDoList: [] // tomorrow Todo List
+    TDTodoList: [], // today Todo list
+    TMTodoList: [], // tomorrow Todo List
+    currentTodo: '',
+    // currentTodoIndex: ''
   },
   methods: {
-    selectType(event){
+    bindOk(event) {
+      let { dateType, TDTodoList, modifyType, fixIndex } = this.data;
+      if (dateType == 1) {
+        this.setData({
+          inputFocus: false
+        })
+        if(modifyType == 1){
+          this.fixTomorrowList();
+        } else {
+          this.addTomorrowList();
+        }
+      }
+    },
+    addTomorrowList() {
+      let { TMTodoList, currentTodo } = this.data;
+      currentTodo && TMTodoList.unshift({
+        todoContent: currentTodo
+      });
+      this.setData({
+        currentTodo: '',
+        // currentTodoIndex: '',
+        TMTodoList
+      })
+    },
+    fixTomorrowList() { 
+      let {fixIndex, currentTodo, TMTodoList} = this.data;
+      TMTodoList[fixIndex].todoContent = currentTodo;
+      this.setData({
+        currentTodo: '',
+        TMTodoList
+      })
+    },
+    selectType(event) {
       let data = event.currentTarget.dataset;
-      let {type} = data;
+      let { type } = data;
       type = Number(!(+type));
       this.setData({
         dateType: type
+      })
+      console.log('dateType', type);
+    },
+    bindKeyInput(event) {
+      let { TMTodoList } = this.data;
+      let inputValue = event.detail.value;
+      // let currentTodoIndex = inputValue.length == 0 ? '' : (TMTodoList.length + 1)
+      this.setData({
+        currentTodo: inputValue,
+        // currentTodoIndex: currentTodoIndex
+      });
+    },
+    fixDoneList(event) {
+      let data = event.currentTarget.dataset;
+      let { TMTodoList } = this.data;
+      let { index } = data;
+      this.setData({
+        currentTodo: TMTodoList[index].todoContent,
+        modifyType: 1,
+        fixIndex: index,
+        inputFocus: true
       })
     }
   }
